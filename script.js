bfd499c2f3796ff457ba643a710972f407cbb86a
Theo-Cupeacoder <delaunetheo@gmail.com>
2022-07-18 16:20:20 +0200

    End of project: Script ended, button new game configured. Modification of rules, english traduction. Add CSS animation for roll dice.

--- script.js
+++ script.js
@@ -1,73 +1,94 @@
-// Add transition for dice
-// Add an indicator for turn
-// Add animation for winner
-// Add event on new game button 
-
 $(document).ready(() => {
   // Set style before playing
-  $('#container').css('display', 'flex').css('justify-content', 'center')
-  $('#game').css('display', 'none')
-  $('#init').css('display', 'flex')
+  $("#container").css("display", "flex").css("justify-content", "center");
+  $("#game").css("display", "none");
+  $("#init").css("display", "flex");
 
-  $('#play').click(() => {
+  $("#play").click(() => {
     // Set style to view the game
-    $('#container').css('display', 'block').css('justify-content', 'none')
-    $('#game').css('display', 'flex')
-    $('#init').css('display', 'none')
+    $("#container").css("display", "block").css("justify-content", "none");
+    $("#game").css("display", "flex");
+    $("#init").css("display", "none");
 
     // Declare variables we need to calculate
-    let totalCurrent = 0
-    let totalPoints = 0
-    let player = 1
+    let totalCurrent = 0;
+    let totalPoints = 0;
+    let player = 1;
+    $(`#indicator-player-${player}`).html(`<div class="indicator"></div>`);
 
+    $("#new-game").click(function () {
+      location.reload();
+    });
 
     // Add event listener on hold button
-    $('#hold').click( function () {
+    $("#hold").click(function () {
       // Calculation of points of current player
-      totalPoints += totalCurrent
-      oldValueTotalPoints = Number($(`#points-${player}`).text())
-      $(`#points-${player}`).text(oldValueTotalPoints + totalPoints)
+      totalPoints += totalCurrent;
+      oldValueTotalPoints = Number($(`#points-${player}`).text());
+      let newValueTotalPoints = oldValueTotalPoints + totalPoints;
+      $(`#points-${player}`).text(newValueTotalPoints);
+      totalCurrent = 0;
+      $(`#current-${player}`).text(totalCurrent);
       // Check if current player win
-      if (Number($(`#points-${player}`).text()) >= 100){
-        alert(`The winner is Player ${player}`)
+      if (Number($(`#points-${player}`).text()) >= 100) {
+        alert(
+          `The winner is Player ${player} with ${newValueTotalPoints} points.`
+        );
+        setTimeout(function () {
+          $("#new-game").trigger("click");
+        }, 1000);
       }
-      // Reset of values / increment player
-      totalPoints = 0
-      totalCurrent = 0
-      player++
+      // Reset values / increment player
+      totalPoints = 0;
+      totalCurrent = 0;
+      $(`#indicator-player-${player}`).hide();
+      player++;
+      $(`#indicator-player-${player}`).html(`<div class="indicator"></div>`);
+      $(`#indicator-player-${player}`).show();
       // Check if player = 3 reset to 1
-      if (player === 3){
-        player = 1
+      if (player === 3) {
+        player = 1;
+        $(`#indicator-player-${player}`).html(`<div class="indicator"></div>`);
+        $(`#indicator-player-${player}`).show();
       }
-
-    })
+    });
     // Add event listener on roll dice button
-    $('#roll-dice').click(function () {
+    $("#roll-dice").click(function () {
       // Function for get random number in a range of 0 to 6
       const randomNumber = function () {
-        result = Math.round(Math.random() * 6)
+        result = Math.round(Math.random() * 6);
         // Retry if result = 0
         while (result === 0) {
-          result = Math.round(Math.random() * 6)
+          result = Math.round(Math.random() * 6);
         }
-        return result
-      }
+        return result;
+      };
       // Define source to view the dice with his value
-      $('#dice').html('<img class="shadow" src="images/Dice-' + randomNumber() + '.png" alt="">')
+      $("#dice").html(
+        '<img class="shadow animated-roll" src="images/Dice-' +
+          randomNumber() +
+          '.png">'
+      );
       // Check if result is different of 1 and calculate current points
       if (result !== 1) {
-        totalCurrent += result
-        $(`#current-${player}`).text(totalCurrent)
+        totalCurrent += result;
+        $(`#current-${player}`).text(totalCurrent);
       } else {
-        totalCurrent = 0
-        $(`#current-${player}`).text(totalCurrent)
-        player++
+        totalCurrent = 0;
+        $(`#current-${player}`).text(totalCurrent);
+        $(`#indicator-player-${player}`).hide();
+        player++;
+        $(`#indicator-player-${player}`).html(`<div class="indicator"></div>`);
+        $(`#indicator-player-${player}`).show();
         // Check if player = 3 reset to 1
-        if (player === 3){
-          player = 1
+        if (player === 3) {
+          player = 1;
+          $(`#indicator-player-${player}`).html(
+            `<div class="indicator"></div>`
+          );
+          $(`#indicator-player-${player}`).show();
         }
       }
-
-    })
-  })
-})
+    });
+  });
+});
